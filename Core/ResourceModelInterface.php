095c33d522d8a22a92f667df05bfc13d1aeef386
nguyenthanh2901 <nguyenthanh2901hust@gmail.com>
2020-11-01 17:06:17 +0700

    k

--- Core/ResourceModelInterface.php
+++ Core/ResourceModelInterface.php
@@ -1,7 +1,8 @@
 <?php
-interface ResourceModelInterface {
+namespace MVC\Core;
+
+interface ResourceModelInterface{
     public function _init($table, $id, $model);
     public function save($model);
     public function delete($model);
 }
-?>
