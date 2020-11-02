f4f94364ec2c30364a09ff563dabc876f3f6cf4a
nguyenthanh2901 <nguyenthanh2901hust@gmail.com>
2020-11-03 00:17:26 +0700

    TRM

---
+++ Models/TaskResourceModel.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace MVC\Models;
+
+use MVC\Models\TaskModel;
+use MVC\Core\ResourceModel;
+
+class TaskResourceModel extends ResourceModel
+{
+    public function __construct()
+    {
+        $this->_init('tasks','id',new TaskModel);
+    }
+}
